34ee4e82d188ed6dd982178aed67492f7400cd6d
istiakabir~ <istiakabirtrofder@gmail.com>
2022-09-05 21:51:05 +0600

    social icon

--- dist/app.56908c73.js
+++ dist/app.56908c73.js
@@ -6395,7 +6395,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "9812" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8721" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
