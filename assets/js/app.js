187e3e8595b91f0c0531b53283acf93c72a6af5e
istiakabir~ <istiakabirtrofder@gmail.com>
2022-09-05 18:48:26 +0600

    svg animation done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+import gsap from "gsap";
 const bar = document.querySelector(".loading-bar-inner");
 const counter =document.querySelector(".loading-counter-number");
 
@@ -10,5 +11,41 @@ let barInterval = setInterval(() => {
 
     if(c===101){
         clearInterval(barInterval)
-    }
+   
+gsap.to(".loading-bar", {
+    duration: 5,
+    rotate: "90deg",
+    left: "1000%",
+});
+
+gsap.to(".loading-text,.loading-counter", {
+    duration: 0.5,
+    opacity:0, 
+
+});
+gsap.to(".loading-box", {
+    duration: 1,
+    height:"500px",
+    borderRadius: "50%" 
+
+});
+gsap.to(".svg-logo", {
+    duration: 10,
+    opacity: 1,
+
+});
+gsap.to(".loading-box", {
+    delay: 2,
+    border: "none",
+
+});
+gsap.to(".loading", {
+    delay: 2,
+    duration: 2,
+    // zIndex: 1,
+    background: "transparent",
+    opacity: 0.5,
+});
+
+}
 }, 20);
